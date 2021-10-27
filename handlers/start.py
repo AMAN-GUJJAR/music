bdb991ba9812ab17a9c0cb5781f95ff88eda7684
DARKAMAN5 <86722987+DARKAMAN5@users.noreply.github.com>
2021-10-27 14:17:58 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -4,69 +4,70 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import BOT_USERNAME
 
 
-@Client.on_message(filters.command(["start", "start@DARKXV2BOT"]) & filters.private & ~filters.channel)
+@Client.on_message(filters.command(["start", "start@ALEXAMUSIC_ROBOT"]) & filters.private & ~filters.channel)
 async def start(_, message: Message):
+    await message.reply_photo("https://te.legra.ph/file/9ee01453fc404013fabf1.jpg")
     await message.reply_text(
-        text="**ʜᴇʟʟᴏ 👋🏻 {}!**\n\n *I'ᴍ 𓆩ᴅᴀʀᴋᴍᴜsɪᴄ𓆪..ᴀɴ ᴀᴍᴀᴢɪɴɢ ɢʀᴏᴜᴘ ᴍᴜsɪᴄ ʙᴏᴛ ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘs..[😉]!**\n\n**ᴄʟɪᴄᴋ /cmdlist ғᴏʀ ᴍᴏʀᴇ ʜᴇʟᴘ ᴏɴ ᴍʏ ᴜsᴀɢᴇ ❤**".format(message.from_user.mention),
+        text="**𝙷𝙴𝙻𝙻𝙾 👋🏻 {}!**\n\n𝙸 **𝙲𝙰𝙽 𝙿𝙻𝙰𝚈 𝙼𝚄𝚂𝙸𝙲 𝙸𝙽 𝚅𝙾𝙸𝙲𝙴 𝙲𝙷𝙰𝚃 𝙾𝙵 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 𝙶𝚁𝙾𝚄𝙿𝚂.**𝙸 𝙷𝙰𝚅𝙴 𝙰 **𝙻𝙾𝚃 𝙾𝙵 𝙲𝙾𝙾𝙻 𝙵𝙴𝙰𝚃𝚄𝚁𝙴 𝚃𝙷𝙰𝚃 𝚆𝙸𝙻𝙻 𝙰𝙼𝙰𝚉𝙴 𝚈𝙾𝚄!**\n\n**𝙲𝙻𝙸𝙲𝙺 /cmdlist 𝙵𝙾𝚁 𝙼𝙾𝚁𝙴 𝙷𝙴𝙻𝙿 𝙾𝙽 𝙼𝚈 𝚄𝚂𝙰𝙶𝙴 ⚡**".format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton("❀ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ❀", url="https://t.me/DARKXV2BOT?startgroup=true")
+            InlineKeyboardButton("🖱️ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 🖱️", url="https://t.me/ALEXAMUSIC_ROBOT?startgroup=true")
             ],[
-            InlineKeyboardButton("❀ɢʀᴏᴜᴘ❀", url="https://t.me/DARKV2SUPPORT"),
-            InlineKeyboardButton("❀ᴄʜᴀɴɴᴇʟ❀", url="https://t.me/dark5_spammer")
+            InlineKeyboardButton("🥳 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 🥳", url="https://t.me/DARKAMANSUPPORT"),
+            InlineKeyboardButton("♨️ 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ♨️", url="https://t.me/DARKAMANCHANNEL")
             ],[
-            InlineKeyboardButton("❀ᴄʀᴇᴀᴛᴇʀ❀", url="http://t.me/DARKAMAN")
+            InlineKeyboardButton("⚡ 𝙰𝙻𝙴𝚇𝙰 𝚁𝙾𝙱𝙾𝚃 ⚡", url="https://t.me/ALEXA_MANAGER_ROBOT")
             ]]
         ),
         disable_web_page_preview=True
     )
         
-@Client.on_message(filters.command(["start", "start@DARKXV2BOT"]) & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command(["start", "start@ALEXAMUSIC_ROBOT"]) & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
     await message.reply_text(
-        text="**❀ᴅᴀʀᴋᴍᴜsɪᴄʙᴏᴛ ɪs ᴏɴʟɪɴᴇ❀**",
+        text="**𝙰𝙻𝙴𝚇𝙰 𝙼𝚄𝚂𝙸𝙲 𝙱𝙾𝚃 𝙸𝚂 𝙾𝙽𝙻𝙸𝙽𝙴 **",
         reply_markup=InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton(text="❀sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ❀", url="https://t.me/DARKV2SUPPORT")
+            InlineKeyboardButton(text="⚡𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿⚡", url="https://t.me/DARKAMANSUPPORT")
             ]]
         )
     )
 
 
-@Client.on_message(filters.command(["cmdlist", "start@DARKXV2BOT"]) & filters.private & ~filters.channel)
+@Client.on_message(filters.command(["cmdlist", "start@ALEXAMUSIC_ROBOT"]) & filters.private & ~filters.channel)
 async def cmdlist(_, message: Message):
     await message.reply_text(
-        text="""**𓆩ᴅᴀʀᴋᴍᴜsɪᴄʙᴏᴛ𓆪 :ʜᴇʟᴘ ᴍᴇɴᴜ**
+        text="""**𝙰𝙻𝙴𝚇𝙰 𝙼𝚄𝚂𝙸𝙲: Help Menu**
 
-__×𓆩ᴀ𓆪 ❀ғɪʀSᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ..
-__×𓆩ᴀ𓆪 ❀ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀS ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜ ɢʀᴏᴜᴘ ᴡɪᴛʜ ᴀʟʟ ᴘᴇʀᴍɪSSɪᴏɴ❀ ..__
+__× First Add Me To Your Group..
+× Promote Me As Admin In Your Group With All Permission..__
 
-**𓆩ᴀ𓆪 ❀ᴄᴏᴍᴍᴏɴ ᴄᴏᴍᴍᴀɴᴅs❀.**
+**🏷 Common Commands.**
 
-• `/ᴘʟᴀʏ` - Sᴏɴɢ ɴᴀᴍᴇ : __ᴘʟᴀʏ ᴠɪᴀ ʏᴏᴜᴛᴜʙᴇ__
-• `/ᴅᴘʟᴀʏ` - Sσηg ɴᴀᴍᴇ : __ᴘʟᴀʏ ᴠɪᴀ ᴅᴇᴇᴢᴇʀ__
-• `/Sᴘʟᴀʏ` - Sᴏɴɢ ɴᴀᴍᴇ : __ᴘʟᴀʏ ᴠɪᴀ ᴊɪᴏ sᴀᴀᴠɴ__
-• `/ᴘʟᴀʏʟɪsᴛ` - __sʜᴏᴡ ɴᴏᴡ ᴘʟᴀʏʟɪsᴛ__
-• `/ᴄᴜʀʀᴇɴᴛ` - __sʜᴏᴡ ɴᴏᴡ ᴘʟᴀʏɪɴɢ__
+• `/play` - Song Name : __Plays Via Youtube__
+• `/dplay` - Song Name : __Play Via Deezer__
+• `/splay` - Song Name : __Play Via Jio Saavn__
+• `/playlist` - __Show now playing list__
+• `/current` - __Show now playing__
 
-• `/sᴏɴɢ` - sᴏɴɢ ɴᴀᴍᴇ : __ɢᴇᴛ ᴛʜᴇ sᴏɴɢ ғʀᴏᴍ ʏᴏᴜᴛᴜʙᴇ__
-• `/vid` - Video Name : __ɢᴇᴛ ᴛʜᴇ sᴏɴɢ ғʀᴏᴍ ʏᴏᴜᴛᴜʙᴇ__
-• `/deezer` - song name : __ᴅᴏᴡɴʟᴏᴀᴅ sᴏɴɢs ʏᴏᴜ ᴡᴀɴᴛs ғʀᴏᴍ ᴅᴇᴇᴢᴇʀ__
-• `/saavn` - song name : __ᴅᴏᴡɴʟᴏᴀᴅ sᴏɴɢ ʏᴏᴜ ᴡᴀɴᴛ ғʀᴏᴍ sᴀᴀᴠɴ__
-• `/search` - YouTube Title : __(ɢᴇᴛ ʏᴏᴜᴛᴜʙᴇ sᴇᴀʀᴄʜ ǫᴜᴇʀʏ)__
+• `/song` - Song Name : __Get The Song From YouTube__
+• `/vid` - Video Name : __Get The Video From YouTube__
+• `/deezer` - song name : __download songs you want quickly via deezer__
+• `/saavn` - song name : __download songs you want quickly via saavn__
+• `/search` - YouTube Title : __(Get YouTube Search Query)__
 
-**𓆩ᴀ𓆪 ❀ɢʀᴏᴜᴘ ᴀᴅᴍɪɴs ᴄᴏᴍᴍᴀɴᴅ❀.**
+**🏷 Group Admin Commands.**
 
-• `/Sᴋɪᴘ : Sᴋɪᴘs ᴍᴜsɪᴄ
-• `/ᴘᴀᴜSᴇ : ᴘᴀᴜSᴇ ᴘʟᴀʏɪɴɢ ᴍᴜSɪᴄ
-• `/ʀᴇSᴜᴍᴇ : ʀᴇSᴜᴍᴇ ᴘʟᴀʏɪɴɢ ᴍᴜSɪᴄ
-• `/ᴇɴᴅ : SᴛᴏᴘS ᴘʟᴀʏɪɴɢ ᴍᴜSɪᴄ
-• `/ʀᴇʟᴏᴀᴅ : ʀᴇʟᴏᴀᴅS ᴀᴅᴍɪɴs ʟɪsᴛS
-• `/ᴜSᴇʀʙᴏᴛᴊᴏɪɴ : ᴀSSɪSᴛᴀɴᴛ ᴊᴏɪɴS ᴛʜᴇ ɢʀᴏᴜᴘ
-• `/ᴜSᴇʀʙᴏᴛʟᴇᴀᴠᴇ : ᴀSSɪsᴛᴀɴᴛ ʟᴇᴀᴠᴇS ғʀᴏᴍ ᴛʜᴇ ɢʀᴏᴜᴘ.__""",
+• `/skip` : __Skips Music__
+• `/pause` : __Pause Playing Music__
+• `/resume` : __Resume Playing Music__
+• `/end` : __Stops playing Music__
+• `/reload` : __Reloads Admin List__
+• `/userbotjoin` : __Assistant Joins The Group__
+• `/userbotleave` : __Assistant Leaves From The Group.__""",
         reply_markup=InlineKeyboardMarkup(
               [[
-              InlineKeyboardButton(text="𓆩ɖǟʀӄӼ ʍʊֆɨƈ ɮօȶ𓆪", url="https://t.me/DARKV2SUPPORT")
+              InlineKeyboardButton(text="⚡ 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 ⚡", url="https://t.me/DARKAMANSUPPORT")
               ]]
           )
       )
